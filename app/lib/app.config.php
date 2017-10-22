8c6203295d8822c2c23d0d137cbb17429cc631f7
chaoserene <lucelio.simas@gmail.com>
2017-10-22 17:13:47 -0200

    Smarty

--- app/lib/app.config.php
+++ app/lib/app.config.php
@@ -1,5 +1,13 @@
 <?php
 
+require('/var/www/html/projetos/app/template/Smarty.class.php');
 require_once('config.php');
 require_once('user.class.php');
 require_once('database.class.php');
+
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('/var/www/html/projetos/app/template/smarty/templates');
+$smarty->setCompileDir('/var/www/html/projetos/app/template/smarty/templates_c');
+$smarty->setCacheDir('/var/www/html/projetos/app/template/smarty/cache');
+$smarty->setConfigDir('/var/www/html/projetos/app/template/smarty/configs');
