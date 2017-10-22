b65f9eac25f8236dbb55fd45186a1999b03cc1b7
chaoserene <lucelio.simas@gmail.com>
2017-10-22 17:55:37 -0200

    db

--- app/index.php
+++ app/index.php
@@ -5,6 +5,9 @@ require_once('lib/app.config.php');
 $now = date('Y-m-d H:i:s');
 $user = new User("chaoserene", "asc3ndant", "lucelio.simas@gmail.com", $now);
 
-$smarty->assign('luc', $user);
+$user->registerUser();
+$registered = $user->isUserRegistered();
+
+$smarty->assign('luc', $registered);
 $smarty->assign('name', 'Luc');
 $smarty->display('name.tpl');
